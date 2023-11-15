4bf9df916ede7f0f4ac970ca6f381e6f016c65de
Aakeefah Jones <aakeefahj@gmail.com>
2023-11-15 20:57:17 +0200

    explanation added

--- script/code.js
+++ script/code.js
@@ -1,8 +1,18 @@
 
-
+// define the array
 function getMiddle(array) {
+
+// array.length represents the number of elements
+// >> basically says move to the right.
     const mid = array.length >> 1;
+
+// '?' shortens the if or else statement.
     return array.length & 1 ? array[mid]
+
+// says to display the thing basically...
+// slice is used to returns elements as a new array. 
         : array.slice(mid - 1, mid + 1);
     }
+
+// display the answer in the console.
     console.log(getMiddle([12, 18, 11, 5, 8, 2]));
